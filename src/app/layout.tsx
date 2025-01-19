9e87ca4f1ce425b52e6aca2287c7bc773d68443e
Dev750wala <sadisatsowaladev1@gmail.com>
2025-01-19 10:54:24 +0530

    feat: update layout metadata and add Ethereum logo for Dev Raffle DApp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,11 @@ import { ThirdwebProvider } from "thirdweb/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
-  description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+  title: "Dev Raffle",
+  description: "Raffle DApp",
+  icons: {
+    icon: "../../public/ethereum-eth-logo.svg",
+  }
 };
 
 export default function RootLayout({
